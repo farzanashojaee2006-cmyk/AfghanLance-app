4c8a2618c138468e20398cee700daee05de81c26
Farzana <farzanashojaee2006@gmail.com>
2026-04-27 21:08:01 +0430

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:afghanlance/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AfghanLance());
+}
+
+class AfghanLance extends StatelessWidget {
+  const AfghanLance({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
+
