76f59aacaae700032ee11f231f6db78e96c845ea
Farzana <farzanashojaee2006@gmail.com>
2026-05-06 07:53:00 +0430

    feat: add home and selection screens with constants

---
+++ lib/Conestant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const  primaryColor = Color(0xFF1D837F);
+const  backgroundColor = Color(0xFFD7EAEE);
+const  textDarkColor = Color(0xFF1F1F1F);
+const  textLightColor = Color(0xFF666666);
+
+
---
+++ lib/SkillCard.dart
@@ -0,0 +1,43 @@
+import 'package:afghanlance/Conestant.dart';
+import 'package:flutter/material.dart';
+
+class SkillCard extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  SkillCard({super.key, required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 90,
+      margin: EdgeInsets.only(right: 12),
+      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.08),
+            blurRadius: 8,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 32, color: primaryColor),
+          SizedBox(height: 8),
+          Text(
+            label,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 12.5, fontWeight: FontWeight.w500),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ],
+      ),
+    );
+  }
+}
